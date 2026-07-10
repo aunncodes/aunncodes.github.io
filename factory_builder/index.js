0ea318e44af7e9e1f546241457cb62e90878bf72
Aunn <choprasahil.sc@gmail.com>
2026-07-09 17:22:01 -0700

    Revert "rrrrrr"
    This reverts commit ccc0147ca403dc0ada6362a53430356287ac4738.

--- factory_builder/index.js
+++ factory_builder/index.js
@@ -31,7 +31,7 @@ const recipe_type_to_block = {
     "crusher": "fb_crusher",
     "enchanter": "fb_enchanter",
     "explosive_furnace": "fb_explosive_furnace",
-    "processor": "fb_processor"
+    "crafter_5": "fb_crafter_5"
 }
 var recipe_lines = [];
 
@@ -202,7 +202,7 @@ function render_recipe(end_item)
             });
             break;
         }
-        case "processor":
+        case "crafter_5":
         {
             div = document.createElement("div");
             div.classList = "recipe crafter crafter_5";
